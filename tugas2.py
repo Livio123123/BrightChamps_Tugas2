d8d93798d31973b747166655f425f42c8c0acc95
Livio123123 <99071165+Livio123123@users.noreply.github.com>
2022-02-05 15:50:34 +0700

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,19 @@
+
+a = input("angka1: ")
+b = input("angka2: ")
+c = input("angka3: ")
+
+if a>b:
+    if a>c:
+        print (a)
+
+if b>a:
+    if b>c:
+        print (b)
+
+if c>b:
+    if a>b:
+        print (c)
+
+
+
